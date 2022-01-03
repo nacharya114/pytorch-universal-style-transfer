75c0ed8008553db6224d0baadf85e97e40b0d1e4
Neil Acharya <nacharya114@gmail.com>
2022-01-03 04:23:12 +0000

    config update

--- universal_nst/config.py
+++ universal_nst/config.py
@@ -1,11 +1,11 @@
 config = {
     "BATCH_SIZE": 8,
     "LEARNING_RATE": 0.01,
-    "EPOCHS": 1,
+    "EPOCHS": 20,
     "LAMBDA": 1,
     "SAVE_MODEL_EVERY": 500,
     # "TRAIN_DATASET_PATH": "./notebooks/dataset/train/train2014/",
     # "SAVE_MODEL_PATH": "./notebooks/model/"
-    "SAVE_MODEL_PATH": "/gcs/nacharya114-models/vgg/autoencoder/"
+    "SAVE_MODEL_PATH": "/gcs/nacharya114-models/vgg/autoencoder/",
     "TRAIN_DATASET_PATH": "/gcs/nacharya114-data/unsupervised/coco/train/"
 }
